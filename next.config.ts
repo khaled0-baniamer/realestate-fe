e409f0f26856ecfb7825ba07dd19b1734cb84fbf
khaled <khaled@slise.ae>
2025-05-31 18:53:21 +0300

    fix base path

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,10 @@ const nextConfig: NextConfig = {
   env: {
     API_URL: process.env.NEXT_PUBLIC_API_ENDPOINT,
   },
-  images:{
-    domains:['f005.backblazeb2.com']
-  }
+  images: {
+    domains: ["f005.backblazeb2.com"],
+  },
+  basePath: "/realestate",
 };
 
 export default nextConfig;
